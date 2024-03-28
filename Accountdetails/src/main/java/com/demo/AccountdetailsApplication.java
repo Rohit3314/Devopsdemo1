80bcd173a161958bf5e12925c8e5bd3396ca8298
Rohit3314 <96432720+Rohit3314@users.noreply.github.com>
2024-03-28 19:38:14 +0530

    registering user module

---
+++ Accountdetails/src/main/java/com/demo/AccountdetailsApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountdetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountdetailsApplication.class, args);
+	}
+
+}
